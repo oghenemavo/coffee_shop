79b51e271dc102b4c419c878fc220e19d4e2ae6e
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2022-10-12 17:49:02 +0100

    CHANGES MADE

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-hvteb963.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-hvteb963.us', // the auth0 domain prefix
     audience: 'image', // the audience set for the auth0 app
     clientId: 'fu2AILfgCOJ6DqKaNXyeYlU9rCofWM5N', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
